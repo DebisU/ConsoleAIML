6ab67653c4036a27659cae1096a6b46b759c38ea
DebisU Uchiha <davis5593@hotmail.com>
2017-08-24 17:17:29 +0200

    Add project files.

---
+++ ConsoleAIML/Lucy.cs
@@ -0,0 +1,44 @@
+﻿using AIMLbot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAIML
+{
+    public class Lucy
+    {
+        const string UserId = "DebisU";
+        private Bot AimlBot;
+        private User myUser;
+
+        public Lucy()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            AimlBot = new Bot();
+            myUser = new User(UserId, AimlBot);
+            Console.Title = "Lucy AIML";
+            Initialize();
+        }
+
+        // Loads all the AIML files in the \AIML folder         
+        public void Initialize()
+        {
+            AimlBot.loadSettings();
+            AimlBot.isAcceptingUserInput = false;
+            AimlBot.loadAIMLFromFiles();
+            AimlBot.isAcceptingUserInput = true;
+        }
+
+        // Given an input string, finds a response using AIMLbot lib
+        public String getOutput(String input)
+        {
+            Request r = new Request(input, myUser, AimlBot);
+            Result res = AimlBot.Chat(r);
+            return (res.Output);
+        }
+    }
+}
---
+++ ConsoleAIML/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAIML
+{
+    class Program
+    {
+        static Lucy bot;
+        static void Main(string[] args)
+        {
+            string input = "start";
+            while (input != "exit")
+            {
+
+                bot = new Lucy();
+                Console.Write("You: ");
+                input = Console.ReadLine();
+                var output = bot.getOutput(input);
+
+                //Console.WriteLine(input);
+                Console.WriteLine("Bot: " + output);
+
+                //Make the bot Speak
+                SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+                synthesizer.Volume = 100;  // 0...100
+                synthesizer.Rate = -2;     // -10...10
+
+                // Synchronous
+                synthesizer.Speak(output);
+
+                // Asynchronous
+                // synthesizer.SpeakAsync(output);
+
+            }
+        }
+    }
+}
